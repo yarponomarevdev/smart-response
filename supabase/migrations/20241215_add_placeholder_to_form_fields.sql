df87adeb518bec5885333858d79f1ec9ee05108e
Ярослав <yaroslavponomarev10@gmail.com>
2025-12-28 16:03:52 +0400

    chore: Удален SQL файл, добавляющий колонку placeholder для кастомных полей формы

--- supabase/migrations/20241215_add_placeholder_to_form_fields.sql
+++
@@ -1,6 +0,0 @@
--- Добавление колонки placeholder для кастомных полей формы
-ALTER TABLE form_fields ADD COLUMN IF NOT EXISTS placeholder text;
-
-COMMENT ON COLUMN form_fields.placeholder IS 'Плейсхолдер для поля ввода';
-
-
